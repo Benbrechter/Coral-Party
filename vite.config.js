6b3855b2219b404a0f240971429de30f339a414e
Benjamin Brechter <benbrechter3@gmail.com>
2025-04-07 16:00:39 -0500

    fwm

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 // Import Tailwind correctly
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    tailwindcss(),
   ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ]
-    }
-  },
-  assetsInclude: ["**/*.PNG"]
-})
+ assetsInclude: ["**/*.PNG"]
+}) 
